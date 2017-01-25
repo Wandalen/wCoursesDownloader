bfc7fce686d7b978a922659f89dfb0d24292a97a
volodymyrmigdal <volodymyr.migdal@gmail.com>
2017-01-25 15:47:31 +0200

    add _login

--- staging/atop/coursesDownloader/CoursesDownloader.s
+++ staging/atop/coursesDownloader/CoursesDownloader.s
@@ -61,6 +61,55 @@ var init = function( o )
   if( o )
   self.copy( o );
 
+  if( !self.request )
+  self.request = require('request');
+
+}
+
+//
+
+var _login = function()
+{
+  var self = this;
+  var request = self.request.defaults({ jar: true });
+
+  var _getCSRF3 = function( cookies )
+  {
+    var src =  cookies[ 0 ];
+    src = src.split( ';' )[ 0 ];
+    src = src.split( '=' );
+
+    var token = src.pop();
+
+    console.log("CSRF3 Token : ", token );
+
+    return token;
+  }
+
+  request('https://www.coursera.org/?authMode=login', function ( err, res, body )
+  {
+  var cookies = res.headers['set-cookie'];
+  var csrf3_token = _getCSRF3( cookies );
+  request.post
+  ({
+    url:'https://www.coursera.org/api/login/v3Ssr?csrf3-token=' + csrf3_token,
+    form:
+    {
+      email : self.email,
+      password : self.password
+    }
+   },
+   function( err, res, body)
+   {
+     if( !err )
+     request( 'https://www.coursera.org/account/profile', function ( err, res, body )
+     {
+       console.log( body );
+     });
+   })
+  });
+
+
 }
 
 // --
@@ -69,10 +118,13 @@ var init = function( o )
 
 var Composes =
 {
+  request : null
 }
 
 var Associates =
 {
+  email : 'wcoursera@gmail.com',
+  password : '17159922',
 }
 
 var Restricts =
@@ -91,6 +143,8 @@ var Proto =
 {
 
   init : init,
+
+  _login : _login,
 
   // relationships
 
