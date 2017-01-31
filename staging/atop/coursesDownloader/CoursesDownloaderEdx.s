aa6cdb423c2da9244febb291dc42f571bbe77ac0
volodymyrmigdal <volodymyr.migdal@gmail.com>
2017-01-31 19:20:24 +0200

    add wCoursesDownloaderEdx

---
+++ staging/atop/coursesDownloader/CoursesDownloaderEdx.s
@@ -0,0 +1,110 @@
+( function _CoursesDownloader_s_( ) {
+
+'use strict';
+
+// dependencies
+
+if( typeof module !== 'undefined' )
+{
+}
+
+// constructor
+
+var _ = wTools;
+var Parent = wCoursesDownloader;
+var Self = function wCoursesDownloaderEdx( o )
+{
+  if( !( this instanceof Self ) )
+  if( o instanceof Self )
+  return o;
+  else
+  return new( _.routineJoin( Self, Self, arguments ) );
+  return Self.prototype.init.apply( this,arguments );
+}
+
+Self.nameShort = 'CoursesDownloaderEdx';
+
+// --
+// inter
+// --
+
+function init( o )
+{
+  var self = this;
+  Parent.prototype.init.call( self,o );
+}
+
+//
+
+// --
+// relationships
+// --
+
+var Composes =
+{
+  verbosity : 1,
+}
+
+var Aggregates =
+{
+}
+
+var Associates =
+{
+  _requestAct : null,
+}
+
+var Restricts =
+{
+}
+
+var Statics =
+{
+}
+
+// --
+// proto
+// --
+
+var Proto =
+{
+
+  init : init,
+
+
+  // relationships
+
+  constructor : Self,
+  Composes : Composes,
+  Aggregates : Aggregates,
+  Associates : Associates,
+  Restricts : Restricts,
+  Statics : Statics,
+
+};
+
+// define
+
+_.protoMake
+({
+  constructor : Self,
+  parent : Parent,
+  extend : Proto,
+});
+
+
+// accessor
+
+_.accessor( Self.prototype,
+{
+});
+
+// readonly
+
+_.accessorReadOnly( Self.prototype,
+{
+});
+
+_.CoursesDownloader.register( Self );
+
+})();
